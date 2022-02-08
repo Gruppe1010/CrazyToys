dc414ae12310c2d82715c256b935d4a6c515f0f4
VibeJensen <vibej@hotmail.com>
2022-02-08 09:49:06 +0100

    Category-entity tilføjet

---
+++ CrazyToys/CrazyToys/Models/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using CrazyToys.Web.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CrazyToys.Web.Models.Entities
+{
+    public class Category
+    {
+        [Column("CategoryId")]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public ICollection<SubCategory> SubCategories { get; set; }
+
+        // TODO skal denne have en collection af Category?
+
+    }
+}
