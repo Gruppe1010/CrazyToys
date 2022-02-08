7c972253aa3690ea0500eb95a089ed63d43776b8
VibeJensen <vibej@hotmail.com>
2022-02-08 13:59:52 +0100

    Tilføjet at den genererer db i Data-projekt

---
+++ CrazyToys/CrazyToys.Data/Data/Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrazyToys.Entities.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CrazyToys.Data.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Colour> Colours { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AgeGroup> AgeGroups { get; set; }
+        public DbSet<Toy> Toys { get; set; }
+
+
+
+
+    }
+}
+
---
+++ CrazyToys/CrazyToys.Data/Startup.cs
@@ -0,0 +1,52 @@
+using CrazyToys.Data.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Data
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+
+        public Startup(IConfiguration config)
+        {
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Context>(options =>
+            options.UseSqlServer(
+                _config.GetConnectionString("context")));
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
