86c185c57d68541e032f990fcd2cbe88835535d5
VibeJensen <vibej@hotmail.com>
2022-02-08 09:48:58 +0100

    context tilføjet med dbset

---
+++ CrazyToys/CrazyToys/Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrazyToys.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CrazyToys.Web.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Colour> Colours { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AgeGroup> AgeGroups { get; set; }
+        public DbSet<Toy> Toys { get; set; }
+
+
+    }
+}
+
