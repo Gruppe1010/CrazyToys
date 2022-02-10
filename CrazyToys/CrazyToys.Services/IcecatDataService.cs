8bcae4fda1d87a4cbbc11b53d369e7679e418e07
VibeJensen <vibej@hotmail.com>
2022-02-10 15:51:24 +0100

    I gang med getSingleProduct i IcecatDataService

--- CrazyToys/CrazyToys.Services/IcecatDataService.cs
+++ CrazyToys/CrazyToys.Services/IcecatDataService.cs
@@ -30,7 +30,7 @@ namespace CrazyToys.Services
             brandDict.Add("23505", new Brand("23505", "Clown"));
             brandDict.Add("32046", new Brand("32046", "Fuzzikins Fuzzi"));
             brandDict.Add("7375", new Brand("7375", "IMC Toys"));
-            brandDict.Add("5669", new Brand("5669", "Hasbro"));
+            brandDict.Add("5669", new Brand("5669", "Hasbro", "https://images.icecat.biz/img/brand/thumb/5669_5c735b62136a4d32b553ed74c66cdb15.jpg"));
             brandDict.Add("24094", new Brand("24094", "LumoStars"));
             brandDict.Add("36068", new Brand("36068", "My Little Pony"));
             brandDict.Add("16933", new Brand("16933", "Play-Doh"));
@@ -40,6 +40,8 @@ namespace CrazyToys.Services
             brandDict.Add("3480", new Brand("3480", "Jumbo"));
         }
 
+
+        // 
         public async Task<Toy> getSingleProduct(string brandId, string productId)
         {
             string username = "alphaslo";
@@ -50,6 +52,7 @@ namespace CrazyToys.Services
 
             string brandName;
             Brand brand;
+            // TODO hent brand op fra db - gem på nyt Toy-obj
             bool hasValue = brandDict.TryGetValue(brandId, out brand);
             if (hasValue)
             {
@@ -82,16 +85,30 @@ namespace CrazyToys.Services
                 string jsonContent =
                     await httpResponseMessage.Content.ReadAsStringAsync();
 
-                var json = JsonConvert.DeserializeObject(jsonContent);
+                dynamic json = JsonConvert.DeserializeObject(jsonContent);
 
-                /*
                 string id = json["data"]["GeneralInfo"]["BrandPartCode"];
                 string name = json["data"]["GeneralInfo"]["Title"];
+                string 
                 string shortDescription = json["data"]["GeneralInfo"]["SummaryDescription"]["ShortSummaryDescription"];
                 string longDescription = json["data"]["GeneralInfo"]["SummaryDescription"]["LongSummaryDescription"];
-                string colourString = json["data"]["GeneralInfo"]["FeatureGroups"]["Features"]["PresentationValue"];
-                */
+                string subCategoryId = json["data"]["GeneralInfo"]["Category"]["CategoryID"];
 
+                // lav tjek på om subCatId allerede er i db - hvis 
+                    // vi var i gang med at snakke om om den ville lave en fejl hvis vi siger add(subcat) og den allerede findes
+                    // TODO tjek om den så overskriver den eller om den laver en fejl - hvis den laver en - 
+                    // dbService.SubCategoryExists()
+
+
+                // colour
+                // stringen skal splittes op i strings og så tilføjes som seperate værdier i colour tabellen, som så skal tilføjes som refs til toy
+                string colourString = json["data"]["GeneralInfo"]["FeatureGroups"]["Features"]["PresentationValue"];
+
+                // agegroups hardcodes i db - lav noget tjek fordi den skal tildeles rigtigt
+
+
+
+                // TODO noget med pris og stock
 
 
 
