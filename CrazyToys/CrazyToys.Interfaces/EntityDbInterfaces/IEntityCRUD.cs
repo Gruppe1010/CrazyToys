962ff0547feb8ca45380d381f45fc595e35563c8
VibeJensen <vibej@hotmail.com>
2022-02-14 11:59:04 +0100

    Navneændring og ændret så GetAll returnerer Liste i stedet for ICollection

--- CrazyToys/CrazyToys.Interfaces/EntityDbInterfaces/IEntityCRUD.cs
+++ CrazyToys/CrazyToys.Interfaces/EntityDbInterfaces/IEntityCRUD.cs
@@ -7,14 +7,13 @@ using System.Threading.Tasks;
 namespace CrazyToys.Interfaces.EntityDbInterfaces
 {
     // I hver service som implementerer dette interface, sender den den type Enititet tilbage i stedet for Object
-    public interface IEnitityCRUD<T>
+    public interface IEntityCRUD<T>
     {
-        Task<ICollection<T>> GetAll();
+        Task<List<T>> GetAll();
 
         Task<T> GetById(string id);
 
         Task<T> GetByName(string name);
-
 
         Task<T> UpdateById(string id);
 
