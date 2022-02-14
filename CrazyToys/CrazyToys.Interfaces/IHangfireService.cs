daa82da67e43f6ececdb17e345b3129f4512a7e0
VibeJensen <vibej@hotmail.com>
2022-02-14 15:38:16 +0100

    IHangfireService og HangfireService oprettet

---
+++ CrazyToys/CrazyToys.Interfaces/IHangfireService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Interfaces
+{
+    public interface IHangfireService
+    {
+        void GetIndex();
+
+        void GetDaily();
+
+    }
+}
---
+++ CrazyToys/CrazyToys.Services/HangfireService.cs
@@ -0,0 +1,24 @@
+﻿using CrazyToys.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Services
+{
+    public class HangfireService : IHangfireService
+    {
+        public IProductDataService ProductDataService { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void GetDaily()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetIndex()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
