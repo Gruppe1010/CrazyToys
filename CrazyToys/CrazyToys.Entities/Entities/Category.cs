7c561320f5e3659c61ff2bbeae67d95efcfcf647
VibeJensen <vibej@hotmail.com>
2022-02-14 14:58:52 +0100

    Tilføjet ny SortingKeywords-prop og field som lægges i db

--- CrazyToys/CrazyToys.Entities/Entities/Category.cs
+++ CrazyToys/CrazyToys.Entities/Entities/Category.cs
@@ -9,11 +9,35 @@ namespace CrazyToys.Entities.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string ID { get; set; }
         public string Name { get; set; }
+
+        [Column("SortingKeywords")]
+        public string _sortingKeywords { get; set; }
+        [NotMapped]
+        public string[] SortingKeywords
+        {
+            get {
+                if (string.IsNullOrWhiteSpace(_sortingKeywords))
+                {
+                    return new string[] {};
+                }
+                return _sortingKeywords.Split("%"); 
+            }
+            set
+            {
+                _sortingKeywords = string.Join("%", value);
+            }
+        }
         public ICollection<SubCategory> SubCategories { get; set; }
 
         public Category(string name)
         {
             Name = name;
+        }
+
+        public Category(string name, string[] sortingKeywords)
+        {
+            Name = name;
+            SortingKeywords = sortingKeywords;
         }
 
         // TODO skal denne have en collection af Category?
