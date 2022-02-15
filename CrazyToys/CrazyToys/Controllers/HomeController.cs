ad3cdfd63a6886025d827653fcb028001ed6db8d
VibeJensen <vibej@hotmail.com>
2022-02-15 15:39:54 +0100

    Vi kalder nu GetIndex() fra Controlleren async

--- CrazyToys/CrazyToys/Controllers/HomeController.cs
+++ CrazyToys/CrazyToys/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-﻿
-using CrazyToys.Interfaces;
-using CrazyToys.Services;
+﻿using CrazyToys.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.Controllers;
@@ -27,14 +24,35 @@ namespace CrazyToys.Web.Controllers
 
         }
 
-        public override IActionResult Index()
+        //
+        // Summary:
+        //     Before the controller executes we will handle redirects and not founds
+
+        /*
+        public HomeController(ILogger<HomeController> logger, ICompositeViewEngine compositeViewEngine, 
+            IUmbracoContextAccessor umbracoContextAccessor, IHangfireService hangfireService)// IProductDataService icecatDataService)
+            //: base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _logger = logger;
+            _compositeViewEngine = compositeViewEngine;
+            _umbracoContextAccessor = umbracoContextAccessor;
+            _hangfireService = hangfireService;
+
+            //_icecatDataService = icecatDataService;
+            _hangfireService = hangfireService;
+
+        }
+        */
+
+
+        public async Task<IActionResult> Blog()
         {
             string test = "gruppe10";
 
             ViewData["Test"] = test;//JsonConvert.SerializeObject(test);
 
-            
-            _hangfireService.GetIndex();
+
+            await _hangfireService.GetIndex();
             /*
             _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("23442", "0401050")).Result;
             _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("5669", "HASB9940EU60")).Result;
@@ -45,6 +63,36 @@ namespace CrazyToys.Web.Controllers
 
             // return a 'model' to the selected template/view for this page.
             return CurrentTemplate(CurrentPage);
+        }
+
+        public override IActionResult Index()
+        {
+            string test = "gruppe10";
+
+            ViewData["Test"] = test;//JsonConvert.SerializeObject(test);
+
+            //var noget = Noget();
+            var getIndexTask = _hangfireService.GetIndex();
+            getIndexTask.Wait();
+            //var result = getIndexTask.Result;
+
+            //await _hangfireService.GetIndex();
+            /*
+            _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("23442", "0401050")).Result;
+            _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("5669", "HASB9940EU60")).Result;
+            _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("15111", "GXB29")).Result;
+            */
+
+
+
+            // return a 'model' to the selected template/view for this page.
+            return CurrentTemplate(CurrentPage);
+        }
+
+        public async Task Noget()
+        {
+            await _hangfireService.GetIndex();
+
         }
     }
 }
