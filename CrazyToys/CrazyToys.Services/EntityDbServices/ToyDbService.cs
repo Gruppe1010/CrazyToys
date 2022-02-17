37757dbac5e0572e78248350b8fd6fd5ce1f897a
VibeJensen <vibej@hotmail.com>
2022-02-17 13:12:21 +0100

    ToyDbService implementer IToyDbService og implementere HasColour()

--- CrazyToys/CrazyToys.Services/EntityDbServices/ToyDbService.cs
+++ CrazyToys/CrazyToys.Services/EntityDbServices/ToyDbService.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace CrazyToys.Services.EntityDbServices
 {
-    public class ToyDbService : IEntityCRUD<Toy>
+    public class ToyDbService : IEntityCRUD<Toy>, IToyDbService
     {
         private readonly Context _context;
 
@@ -31,7 +31,7 @@ namespace CrazyToys.Services.EntityDbServices
         {
             Toy toyFromDb = await GetById(toy.ID);
 
-            if(toyFromDb != null)
+            if (toyFromDb != null)
             {
                 return await Update(toy);
             }
@@ -42,9 +42,9 @@ namespace CrazyToys.Services.EntityDbServices
 
         }
 
-        public Task<List<Toy>> GetAll()
+        public async Task<List<Toy>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _context.Toys.ToListAsync();
         }
 
         public async Task<Toy> GetById(string id)
@@ -72,26 +72,72 @@ namespace CrazyToys.Services.EntityDbServices
             throw new NotImplementedException();
         }
 
+        public async Task<List<Colour>> GetColours(string toyId)
+        {
+            //var cat_id = 1; // Change this variable for your real cat_id
+
+            var query = from colour in _context.Colours
+                        where colour.Toys.Any(t => t.ID.Equals(toyId))
+                        select colour;
+
+            var colours = await query.ToListAsync();
+
+            return colours;
+
+
+        }
+
+        public bool HasColour(string toyId, int colourId)
+        {
+            //var colours = await _context.Toys.Where(toy => toy.ID == toyId && ).ToListAsync();
+            /*
+
+            var noget = _context.Toys
+            .Join(
+                _context.Colours,
+                toy => toy.ID,
+                colour => colour.ID.CustomerId,
+                (customer, invoice) => new
+                {
+                    InvoiceID = invoice.Id,
+                    CustomerName = customer.FirstName + "" + customer.LastName,
+                    InvoiceDate = invoice.Date
+                }
+            ).ToListAsync();
+            */
+
+
+            var nogetAndet = _context.Toys
+            .Where(toy => toy.ID.Equals(toyId))                      // only if you don't want all Users
+            .Select(toy => new
+            {    // Select only the properties you plan to use:
+                Colours = toy.Colours
+                     .Where(colour => colour.ID == colourId)         // only if you don't want all profiles
+                     .Select(colour => new
+                     {
+                         ID = colour.ID
+                     })
+                     .ToList(),
+            });
+
+
+
+            foreach (var item in nogetAndet)
+            {
+            }
+
+
+            var hej = 1;
+            return false;
+        }
+
         public async Task<Toy> Update(Toy toy)
         {
-            try { 
-                _context.Update(toy);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ToyExists(toy.ID))
-                {
-                    // noget
-                }
-               
-            }
+            _context.Update(toy);
+            await _context.SaveChangesAsync();
+
             return toy;
         }
 
-        private bool ToyExists(string id)
-        {
-            return _context.Toys.Any(e => e.ID.Equals(id));
-        }
     }
 }
