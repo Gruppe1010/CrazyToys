e983dfea6a7f57ed5431c53f627b953b4d012a60
VibeJensen <vibej@hotmail.com>
2022-02-17 13:11:46 +0100

    IToyInterface oprettet med HasColour()

---
+++ CrazyToys/CrazyToys.Interfaces/EntityDbInterfaces/IToyDbService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Interfaces.EntityDbInterfaces
+{
+    public interface IToyDbService
+    {
+
+        bool HasColour(string toyId, int colourId);
+
+    }
+}
