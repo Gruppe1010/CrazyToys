26c6ff244b7de41860846c7eff3b2200e0a1113c
VibeJensen <vibej@hotmail.com>
2022-02-18 14:22:36 +0100

    Tilføjet log

---
+++ CrazyToys/CrazyToys.Interfaces/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Interfaces
+{
+    public interface ILog
+    {
+
+        Task WriteExceptionToLog(string className, string methodName, Exception e);
+
+    }
+}
---
+++ CrazyToys/CrazyToys.Services/LogGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Web.Logging
+{
+    public class LogGenerator
+    {
+
+        public async Task WriteExceptionToLog(string className, string methodName,  Exception e)
+        {
+            DateTime timeOfError = DateTime.Now;
+            string dateString = timeOfError.ToString();
+
+            string lines =
+                $"Dato: {dateString}\n" +
+                $"Fil: {className}\n" +
+                $"Metode: {methodName}\n" +
+                $"Exception: {e}\n" +
+                $"ExceptionMessage: {e.Message}\n\n\n";
+
+            using StreamWriter file = new("../CrazyToys/OurLogs/Log.txt", append: true);
+            await file.WriteLineAsync(lines);
+        }
+    }
+}
