91db1aa0fef6a7f851c7803cd24f7f867ecbf9e7
VibeJensen <vibej@hotmail.com>
2022-02-21 12:01:34 +0100

    DateString tilføjet som prop

--- CrazyToys/CrazyToys.Entities/Entities/SimpleToy.cs
+++ CrazyToys/CrazyToys.Entities/Entities/SimpleToy.cs
@@ -14,20 +14,26 @@ namespace CrazyToys.Entities.Entities
         public string SupplierId { get; set; }
         public string ProductId { get; set; }
         public string OnMarket { get; set; }
+        public string Gtin13 { get; set; }
+        public string DateString { get; set; }
 
-        public SimpleToy(string iD, string supplierId, string productId, string onMarket)
+        public SimpleToy(string iD, string supplierId, string productId, string onMarket, string gtin13, string dateString)
         {
             ID = iD;
             SupplierId = supplierId;
             ProductId = productId;
             OnMarket = onMarket;
+            Gtin13 = gtin13;
+            DateString = dateString;
         }
 
-        public SimpleToy(string supplierId, string productId, string onMarket)
+        public SimpleToy(string supplierId, string productId, string onMarket, string gtin13, string dateString)
         {
             SupplierId = supplierId;
             ProductId = productId;
             OnMarket = onMarket;
+            Gtin13 = gtin13;
+            DateString = dateString;
         }
     }
 }
