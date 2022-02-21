c5a725f5b0699be611b9a3685116f4f9d827f0dd
VibeJensen <vibej@hotmail.com>
2022-02-21 09:07:35 +0100

    Logo tilføjet

--- CrazyToys/CrazyToys/Controllers/HomeController.cs
+++ CrazyToys/CrazyToys/Controllers/HomeController.cs
@@ -56,9 +56,11 @@ namespace CrazyToys.Web.Controllers
             string indexUrl = "https://data.Icecat.biz/export/freexml/EN/files.index.xml";
             string dailyUrl = "https://data.Icecat.biz/export/freexml/EN/daily.index.xml";
 
+            /*
             
             var getindextask = _hangfireService.GetProductsFromIcecat(dailyUrl);
             getindextask.Wait();
+            */
             
 
 
