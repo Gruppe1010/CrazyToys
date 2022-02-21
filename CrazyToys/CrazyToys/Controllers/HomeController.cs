857a36d93c342a2e6825674983edaa5437cf2e00
VibeJensen <vibej@hotmail.com>
2022-02-21 13:57:15 +0100

    Whitespace + index

--- CrazyToys/CrazyToys/Controllers/HomeController.cs
+++ CrazyToys/CrazyToys/Controllers/HomeController.cs
@@ -52,16 +52,11 @@ namespace CrazyToys.Web.Controllers
 
             ViewData["Test"] = test;//JsonConvert.SerializeObject(test);
 
-            
             string indexUrl = "https://data.Icecat.biz/export/freexml/EN/files.index.xml";
             string dailyUrl = "https://data.Icecat.biz/export/freexml/EN/daily.index.xml";
-
-            
             
             var getindextask = _hangfireService.GetProductsFromIcecat(indexUrl);
             getindextask.Wait();
-            
-            
 
 
             // hent alle PRODUKTER
@@ -70,28 +65,9 @@ namespace CrazyToys.Web.Controllers
             getIndexTask.Wait();
             */
 
-
-
-
-            //var result = getIndexTask.Result;
-
-            //await _hangfireService.GetIndex();
-            /*
-            _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("23442", "0401050")).Result;
-            _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("5669", "HASB9940EU60")).Result;
-            _ = Task.Run(async () => await _icecatDataService.GetSingleProduct("15111", "GXB29")).Result;
-            */
-
-
-
             // return a 'model' to the selected template/view for this page.
             return CurrentTemplate(CurrentPage);
         }
-
-
-      
-
-
     }
 }
 
