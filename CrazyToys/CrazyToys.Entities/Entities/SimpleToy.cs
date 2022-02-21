6f01e5a435cb05c700b5220d209fab425a8961b8
TobiasBojesen <tobi103i@stud.kea.dk>
2022-02-21 09:22:07 +0100

    SimpleToy Entity oprettet

---
+++ CrazyToys/CrazyToys.Entities/Entities/SimpleToy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Entities.Entities
+{
+    public class SimpleToy
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+        public string SupplierId { get; set; }
+        public string ProductId { get; set; }
+        public bool OnMarket { get; set; }
+
+        public SimpleToy(string iD, string supplierId, string productId, bool onMarket)
+        {
+            ID = iD;
+            SupplierId = supplierId;
+            ProductId = productId;
+            OnMarket = onMarket;
+        }
+    }
+}
