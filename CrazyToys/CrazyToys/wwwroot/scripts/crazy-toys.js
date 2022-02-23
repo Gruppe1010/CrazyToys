1bcdf87b6d41c66f57d704a38915eb8700e3883d
TobiasBojesen <tobi103i@stud.kea.dk>
2022-02-23 10:41:53 +0100

    javascript er oprettet

---
+++ CrazyToys/CrazyToys/wwwroot/scripts/crazy-toys.js
@@ -0,0 +1,5 @@
+﻿var chosenBrand = $('.chosen-brand');
+chosenBrand.on('click', function () {
+    console.log("HEEEEEEJEJEJEJEJEJEj")
+    // lav funktion der viser produkter ud fra hvilke brand der er trykket på
+});
