922f14df62cf8410a107fe18523859296559a54f
TobiasBojesen <tobi103i@stud.kea.dk>
2022-02-23 10:42:44 +0100

    ændret i MaleFashion main.js så når man trykker på pil op i quantity så øger den antallet

---
+++ CrazyToys/CrazyToys/Controllers/ShopDetailsController.cs
@@ -0,0 +1,94 @@
+﻿using CrazyToys.Entities.Entities;
+using CrazyToys.Interfaces;
+using CrazyToys.Interfaces.EntityDbInterfaces;
+using CrazyToys.Services.EntityDbServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace CrazyToys.Web.Controllers
+{
+    public class ShopDetailsController : RenderController
+    {
+        private readonly IHangfireService _hangfireService;
+
+        
+        private readonly IEntityCRUD<Brand> _brandDbService;
+        private readonly IEntityCRUD<Category> _categoryDbService;
+        private readonly IEntityCRUD<Colour> _colourDbService;
+        private readonly ToyDbService _toyDbService;
+        private readonly IEntityCRUD<AgeGroup> _ageGroupDbService;
+       
+
+        public ShopDetailsController(ILogger<HomeController> logger, ICompositeViewEngine compositeViewEngine, 
+            IUmbracoContextAccessor umbracoContextAccessor, IHangfireService hangfireService,
+            IEntityCRUD<Brand> brandDbService, IEntityCRUD<Category> categoryDbService,
+            IEntityCRUD<Colour> colourDbService, ToyDbService toyDbService,
+            IEntityCRUD<AgeGroup> ageGroupDbService)
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _hangfireService = hangfireService;
+            _brandDbService = brandDbService;
+            _categoryDbService = categoryDbService;
+            _colourDbService = colourDbService;
+            _toyDbService = toyDbService;
+            _ageGroupDbService = ageGroupDbService;
+        }
+
+        //
+        // Summary:
+        //     Before the controller executes we will handle redirects and not founds
+
+        public override IActionResult Index()
+        {
+            var noget = 1;
+            /*
+            var getAllCategoriesTask = _categoryDbService.GetAllWithRelations();
+            getAllCategoriesTask.Wait();
+            List<Category> categories = getAllCategoriesTask.Result;
+
+            var getAllAgeGroupsTask = _ageGroupDbService.GetAll();
+            getAllAgeGroupsTask.Wait();
+            List<AgeGroup> ageGroups = getAllAgeGroupsTask.Result;
+            ageGroups.Sort((x, y) => x.Interval[0].CompareTo(y.Interval[0]));
+
+            var getAllBrandsTask = _brandDbService.GetAllWithRelations();
+            getAllBrandsTask.Wait();
+            List<Brand> brands = getAllBrandsTask.Result.OrderBy(o => o.Name).ToList();
+
+            var getAllColoursTask = _colourDbService.GetAll();
+            getAllColoursTask.Wait();
+            List<Colour> colours = getAllColoursTask.Result;
+
+            
+            var getAllToysTask = _toyDbService.GetAllWithRelations();
+            getAllToysTask.Wait();
+            
+            List<Toy> toys = getAllToysTask.Result;
+
+            ViewData["Categories"] = categories;
+            ViewData["AgeGroups"] = ageGroups;
+            ViewData["Brands"] = brands;
+            ViewData["Colours"] = colours;
+            ViewData["Toys"] = toys;
+            */
+
+
+            // return a 'model' to the selected template/view for this page.
+            return CurrentTemplate(CurrentPage);
+        }
+    }
+}
+
+
+
+
+
+   
