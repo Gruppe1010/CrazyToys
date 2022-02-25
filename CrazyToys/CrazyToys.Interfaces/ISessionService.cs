6f0b3db0c1f6c79487f65ae40be1bdf1ccb11a3a
VibeJensen <vibej@hotmail.com>
2022-02-25 14:14:16 +0100

    Interface og SessionService implementeret

---
+++ CrazyToys/CrazyToys.Interfaces/ISessionService.cs
@@ -0,0 +1,11 @@
+﻿using CrazyToys.Entities.Entities;
+using Microsoft.AspNetCore.Http;
+
+
+namespace CrazyToys.Interfaces
+{
+    public interface ISessionService
+    {
+        SessionUser GetNewOrExistingSessionUser(HttpContext httpContext);
+    }
+}
---
+++ CrazyToys/CrazyToys.Services/SessionService.cs
@@ -0,0 +1,29 @@
+﻿using CrazyToys.Entities.DTOs;
+using CrazyToys.Entities.Entities;
+using CrazyToys.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Services
+{
+    public class SessionService : ISessionService
+    {
+        //Tjekker om der allerede er en SessionUser, og returnererer enten den eller opretter en ny, som returneres
+        public SessionUser GetNewOrExistingSessionUser(HttpContext httpContext)
+        {
+            var sessionUser = httpContext.Session.GetString("SessionUser");
+            if (String.IsNullOrWhiteSpace(sessionUser))
+            {
+                SessionUser newSessionUser = new SessionUser();
+                httpContext.Session.SetString("SessionUser", JsonConvert.SerializeObject(newSessionUser));
+                return newSessionUser;
+            }
+            return JsonConvert.DeserializeObject<SessionUser>(sessionUser);
+        }
+    }
+}
