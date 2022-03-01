4384c7b446b8c909619cbb0fe02824c358d7268a
VibeJensen <vibej@hotmail.com>
2022-03-01 16:09:28 +0100

    test post

--- CrazyToys/CrazyToys.Services/SolrService.cs
+++ CrazyToys/CrazyToys.Services/SolrService.cs
@@ -1,7 +1,14 @@
 ﻿using CrazyToys.Interfaces;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SolrNet;
 using SolrNet.Exceptions;
 using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CrazyToys.Services
 {
@@ -9,20 +16,58 @@ namespace CrazyToys.Services
         where TSolrOperations : ISolrOperations<T>
     {
         private readonly TSolrOperations _solr;
+        private readonly IHttpClientFactory _httpClientFactory;
 
 
-        
-        public SolrService(ISolrOperations<T> solr)
+
+
+        public SolrService(ISolrOperations<T> solr, IHttpClientFactory httpClientFactory)
         {
             _solr = (TSolrOperations)solr;
+            _httpClientFactory = httpClientFactory;
         }
         
 
-        public bool CreateOrUpdate(T document)
+        public async Task<bool> CreateOrUpdate(T document)
         {
             try
             {
-                // If the id already exists, the record is updated, otherwise added   
+
+                string url = "https://127.0.0.1:8983/solr/test/update/json/docs?commit=true";
+                /*
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+                {
+                    Headers =
+                    {
+                        { HeaderNames.Accept, "application/json" },
+                    }
+                };
+
+                httpRequestMessage.Content = JsonContent.Create(new { id = "testId", name = "Vores test toy"});
+                */
+
+                JObject payLoad = new JObject(
+                new JProperty("id", "noget"),
+                new JProperty("name", "xxxxxx"));
+           
+                var httpContent = new StringContent(payLoad.ToString(), Encoding.UTF8, "application/json");
+
+                var httpClient = _httpClientFactory.CreateClient();
+                //httpClient.Timeout = TimeSpan.FromMinutes(1000);
+                var httpResponseMessage = await httpClient.PostAsync(url, httpContent);
+                string noget = "hej";
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+
+                    return true;
+
+
+                }
+
+
+
+                    // If the id already exists, the record is updated, otherwise added   
                 _solr.Add(document);
                 _solr.Commit();
                 return true;
