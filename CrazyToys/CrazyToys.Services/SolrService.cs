4f1d5d4b72e2a48c135a03dc1981fd003c56f885
Rasmus Roager <rasm41g0@stud.kea.dk>
2022-03-02 14:19:31 +0100

    Kosmetiske rettelser i SolrService og slettet SolrConnection

--- CrazyToys/CrazyToys.Services/SolrService.cs
+++ CrazyToys/CrazyToys.Services/SolrService.cs
@@ -33,48 +33,50 @@ namespace CrazyToys.Services
         {
             try
             {
-                //SolrToy solrToy = (SolrToy)Convert.ChangeType(document, typeof(SolrToy));
-                //string url = "http://127.0.0.1:8983/solr/test/update/json/docs?commit=true";
-
-                ////var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, url)
-                ////{
-                ////    Headers =
-                ////    {
-                ////        { HeaderNames.Accept, "application/json" },
-                ////    }
-                ////};
-
-                ////httpRequestMessage.Content = JsonContent.Create(new { id = "testId", name = "Vores test toy"});
-
-                //JObject payLoad = new JObject(
-                //new JProperty("id", "noget1"),
-                //new JProperty("name", "xxxxxx"));
-
-                //var httpContent = new StringContent(payLoad.ToString(), Encoding.UTF8, "application/json");
-
-                ////HttpClientHandler clientHandler = new HttpClientHandler();
-                ////clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-                ////// Pass the handler to httpclient(from you are calling api)
-                ////HttpClient httpClient = new HttpClient(clientHandler);
-
-                //var httpClient = _httpClientFactory.CreateClient();
-                //httpClient.Timeout = TimeSpan.FromMinutes(1000);
-                //var httpResponseMessage = await httpClient.PostAsync(url, httpContent);
-                //string noget = "hej";
-
-                ////if (httpResponseMessage.IsSuccessStatusCode)
-                ////{
-
-                ////    return true;
-
-
-                ////}
-
                 // If the id already exists, the record is updated, otherwise added   
                 _solr.Add(document);
                 _solr.Commit();
                 return true;
+
+                /*
+                SolrToy solrToy = (SolrToy)Convert.ChangeType(document, typeof(SolrToy));
+                string url = "http://127.0.0.1:8983/solr/test/update/json/docs?commit=true";
+
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+                {
+                    Headers =
+                    {
+                        { HeaderNames.Accept, "application/json" },
+                    }
+                };
+
+                httpRequestMessage.Content = JsonContent.Create(new { id = "testId", name = "Vores test toy" });
+
+                JObject payLoad = new JObject(
+                new JProperty("id", "noget1"),
+                new JProperty("name", "xxxxxx"));
+
+                var httpContent = new StringContent(payLoad.ToString(), Encoding.UTF8, "application/json");
+
+                HttpClientHandler clientHandler = new HttpClientHandler();
+                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+                // Pass the handler to httpclient(from you are calling api)
+                HttpClient httpClient = new HttpClient(clientHandler);
+
+                var httpClient = _httpClientFactory.CreateClient();
+                httpClient.Timeout = TimeSpan.FromMinutes(1000);
+                var httpResponseMessage = await httpClient.PostAsync(url, httpContent);
+                string noget = "hej";
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+
+                    return true;
+
+
+                }
+                */
             }
             catch (Exception ex)
             {
