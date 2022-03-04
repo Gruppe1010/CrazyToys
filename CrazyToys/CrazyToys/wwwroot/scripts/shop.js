801835cc119ace8b04a594f616ac4726db69e313
VibeJensen <vibej@hotmail.com>
2022-03-04 13:53:20 +0100

    Tilføjet

---
+++ CrazyToys/CrazyToys/wwwroot/scripts/shop.js
@@ -0,0 +1,19 @@
+﻿
+
+// http://solr:8983/solr/test/select?indent=true&q.op=OR&q=brand%3A%22Barbie%22
+
+
+var ulBrand = $('#ulBrand');
+
+fetch("http://localhost:8983/solr/test/select?facet.field=brand&facet=true&indent=true&q.op=OR&q=*%3A*&rows=0")
+    .then(reponse => reponse.json())
+    .then(data => console.log(data));
+
+
+
+
+var chosenBrand = $('.chosen-brand');
+chosenBrand.on('click', function () {
+    console.log("HEEEEEEJEJEJEJEJEJEj")
+    // lav funktion der viser produkter ud fra hvilke brand der er trykket på
+});
