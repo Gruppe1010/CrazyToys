fbd1960f78779eca9075c064d20d567981a3668e
Rasmus Roager <rasm41g0@stud.kea.dk>
2022-03-07 10:41:07 +0100

    PriceGroups til føjet og bliver nu initialiseret

--- CrazyToys/CrazyToys.Data/Data/DbInitializer.cs
+++ CrazyToys/CrazyToys.Data/Data/DbInitializer.cs
@@ -57,7 +57,24 @@ namespace CrazyToys.Data.Data
                 }
             }
 
-            if(!context.Categories.Any())
+            if (!context.PriceGroups.Any())
+            {
+                var priceGroups = new PriceGroup[]
+                {
+                    new PriceGroup(0, 100),
+                    new PriceGroup(100, 200),
+                    new PriceGroup(200, 300),
+                    new PriceGroup(300, 400),
+                    new PriceGroup(400, 500),
+                    new PriceGroup(500, 0)
+                };
+                foreach (PriceGroup priceGroup in priceGroups)
+                {
+                    context.PriceGroups.Add(priceGroup);
+                }
+            }
+
+            if (!context.Categories.Any())
             {
                 var categories = new Category[]
                 { 
---
+++ CrazyToys/CrazyToys.Entities/Entities/PriceGroup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrazyToys.Entities.Entities
+{
+    public class PriceGroup
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        // Hvis man laver id-proppen om til int generer den fra 1 og op af
+        public string ID { get; set; }
+        public string Interval { get; set; }
+        public int Minimum { get; set; }
+        public int Maximum { get; set; }
+
+        public PriceGroup(int minimum, int maximum)
+        {
+            Minimum = minimum;
+            Maximum = maximum;
+            // hvis vi ikke giver en max-værdi med skal der stå fx 800+ og ikke 700-800
+            Interval = Maximum != 0 
+                ? Minimum + " - " + Maximum 
+                : Maximum + "+";
+        }
+    }
+}
