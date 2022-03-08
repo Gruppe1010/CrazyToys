eea738b2d893a31389d0983e106cd4e654d2ea07
TobiasBojesen <tobi103i@stud.kea.dk>
2022-03-08 13:22:55 +0100

    CheckoutController : SurfaceController HttpPost submit metode der får en CheckoutUserModel fra form

---
+++ CrazyToys/CrazyToys/Controllers/CheckoutController.cs
@@ -0,0 +1,38 @@
+﻿using CrazyToys.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Common.Filters;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace CrazyToys.Web.Controllers
+{
+    public class CheckoutController : SurfaceController
+
+    {
+        public CheckoutController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, 
+            ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) 
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+
+        [HttpPost]
+        public IActionResult Submit(CheckoutUserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            // Work with form data here
+            var noget = model.Firstname;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
