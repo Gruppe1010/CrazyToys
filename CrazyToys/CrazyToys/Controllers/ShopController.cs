811a46ad24148a16ab25b51043dc459a859575ed
VibeJensen <vibej@hotmail.com>
2022-03-14 10:52:28 +0100

    Ændret ageGroup til ageGroupInterval og colour til colours

--- CrazyToys/CrazyToys/Controllers/ShopController.cs
+++ CrazyToys/CrazyToys/Controllers/ShopController.cs
@@ -59,18 +59,18 @@ namespace CrazyToys.Web.Controllers
 
         [HttpGet]
         public async Task<IActionResult> Index(
-            [FromQuery(Name = "categories")] string category, // én
-            [FromQuery(Name = "subCategory")] string subCategory, // én
+            [FromQuery(Name = "categories")] string category,
+            [FromQuery(Name = "subCategory")] string subCategory,
             [FromQuery(Name = "brand")] string brand,
             [FromQuery(Name = "priceGroup")] string priceGroup,
-            [FromQuery(Name = "ageGroups")] string ageGroup,
-            [FromQuery(Name = "colours")] string colour,
+            [FromQuery(Name = "ageGroupInterval")] string ageGroupInterval,
+            [FromQuery(Name = "colours")] string colours,
             [FromQuery(Name = "p")] string page,
             [FromQuery(Name = "search")] string search,
             [FromQuery(Name = "sort")] string sort)
         {
 
-            Dictionary<int, List<ShopToyDTO>> dict = await _solrToyService.GetToysForSinglePage(category, subCategory, brand, priceGroup, ageGroup, colour, page, search, sort);
+            Dictionary<int, List<ShopToyDTO>> dict = await _solrToyService.GetToysForSinglePage(category, subCategory, brand, priceGroup, ageGroupInterval, colours, page, search, sort);
 
             int numFound = dict.ElementAt(0).Key;
             List<ShopToyDTO> shopToyDTOs = dict.ElementAt(0).Value;
@@ -90,7 +90,7 @@ namespace CrazyToys.Web.Controllers
             ViewData["Brands"] = brandDict;
             ViewData["ColourGroups"] = colourGroups.OrderBy(a => a.Name).ToList();
             ViewData["ShopToyDTOs"] = shopToyDTOs;
-            ViewData["ParamsDict"] = JsonConvert.SerializeObject(CreateDictFromParams(category, subCategory, brand, priceGroup, ageGroup, colour, page, search));
+            ViewData["ParamsDict"] = JsonConvert.SerializeObject(CreateDictFromParams(category, subCategory, brand, priceGroup, ageGroupInterval, colours, page, search));
 
             // return a 'model' to the selected template/view for this page.
             return CurrentTemplate(CurrentPage);
