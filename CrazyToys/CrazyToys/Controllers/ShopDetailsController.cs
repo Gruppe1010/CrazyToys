092ca437bc2c6f93f7b10cd17da017a8e71965e0
VibeJensen <vibej@hotmail.com>
2022-03-14 17:36:44 +0100

    Slettet ubrugte using-statements

--- CrazyToys/CrazyToys/Controllers/ShopDetailsController.cs
+++ CrazyToys/CrazyToys/Controllers/ShopDetailsController.cs
@@ -1,14 +1,7 @@
-﻿using CrazyToys.Entities.Entities;
-using CrazyToys.Interfaces;
-using CrazyToys.Interfaces.EntityDbInterfaces;
-using CrazyToys.Services.EntityDbServices;
+﻿using CrazyToys.Services.EntityDbServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.Controllers;
