1f32d1e6259f458577b2e23259030a641cb32f55
VibeJensen <vibej@hotmail.com>
2022-03-15 11:11:15 +0100

    js-kode til inc og dec logik fjernet og tilføjet til shop-details.js i stedet

---
+++ CrazyToys/CrazyToys/wwwroot/scripts/shop-details.js
@@ -0,0 +1,26 @@
+﻿
+function incOrDecChosenAmount(stockAmount, inc) {
+    var chosenAmount = document.getElementById('chosenAmount');
+    var oldValue = chosenAmount.value;
+
+    var newVal;
+
+    if (inc) {
+        if (oldValue < stockAmount) {
+            newVal = parseFloat(oldValue) + 1;
+        } else {
+            newVal = oldValue;
+
+            alert(`Der er kun ${oldValue} stk. tilbage af denne vare`);
+        }
+
+    } else {
+        // Don't allow decrementing below zero
+        if (oldValue > 0) {
+            newVal = parseFloat(oldValue) - 1;
+        } else {
+            newVal = 0;
+        }
+    }
+    chosenAmount.value = newVal;
+}
