a48b738330278caa7c99a3e516b19f0dff971b29
Rasmus Roager <rasm41g0@stud.kea.dk>
2022-03-17 12:12:26 +0100

    Tilføjet ContactController

---
+++ CrazyToys/CrazyToys/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace CrazyToys.Web.Controllers
+{
+    public class ContactController : RenderController
+    {
+ 
+        public ContactController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) 
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+
+        }
+
+        [HttpGet]
+        //[FromQuery bruges til at tage imod query parameter fra url]
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.Current = "Contact";
+
+            return CurrentTemplate(CurrentPage);
+        }
+    }
+}
+
+
+
+
+
+   
