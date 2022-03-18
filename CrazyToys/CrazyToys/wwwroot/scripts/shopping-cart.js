627100935003adf031a61fc216f797d5e6a80d4a
VibeJensen <vibej@hotmail.com>
2022-03-18 11:18:55 +0100

    incAmount virker  i cart og opdaterer total og cart total-section

--- CrazyToys/CrazyToys/wwwroot/scripts/shopping-cart.js
+++ CrazyToys/CrazyToys/wwwroot/scripts/shopping-cart.js
@@ -1,13 +1,67 @@
 ﻿
-function updateCartTotalPrices(shoppingCartToyDTOs) {
+function incAmount(shoppingCartToyDTO) {
+    debugger;
 
-    let subtotal = 0;
+    var amountElement = document.getElementById('chosenAmount');
+    var oldValue = parseFloat(amountElement.value);
+    var newValue = oldValue;
+
+    if (oldValue < shoppingCartToyDTO.Stock) {
+
+        const selectedToy = { ToyID: shoppingCartToyDTO.ID, Quantity: 1 };
+
+        fetch(`https://localhost:44325/api/sessionuser/AddToCart`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8' // denne linje siger at dataen som vi sender er en string 
+            },
+            body: JSON.stringify(selectedToy)
+        }).then(response => {
+            if (response.ok) {
+                // hvis toyet blev incrementet successfuldt på sessionUsers cart, så vis det ude på siden
+                newValue++;
+                amountElement.value = newValue;
+                updateTotal(shoppingCartToyDTO.ID, shoppingCartToyDTO.Price * newValue);
+                updateCartTotal(newValue > oldValue ? shoppingCartToyDTO.Price : 0);
+            } else {
+                throw new Error("Error in incrementing toy in cart");
+            }
+        }).catch(error => console.log);
+
+    } else {
+        alert(`Der er kun ${oldValue} stk. tilbage af denne vare`);
+    }
+}
+
+// TODO indsæt at den også skal rette i SessionUser hver gang man trykker inc eller dec
+
+function decAmount() {
+
+    var amountElement = document.getElementById('chosenAmount');
+    var oldValue = amountElement.value;
+
+    var newVal = oldValue > 1
+        ? parseFloat(oldValue) - 1
+        : 1;
+
+    amountElement.value = newVal;
+
+    updateCartTotal(newValue < oldValue ? -price : 0);
+}
+
+function updateTotal(id, price) {
+    document.getElementById(`total-${id}`).innerText = price + " DKK";
+}
+
+
+function updateCartTotal(priceChange) {
+    debugger;
+
+    let subtotal = parseFloat(document.getElementById('subtotal').innerText.split(" ")[0]);
     let deliveryPrice = 39;
 
-    shoppingCartToyDTOs.forEach(shoppingCartToyDTO => {
-        subtotal = subtotal + shoppingCartToyDTO.Price * shoppingCartToyDTO.Quantity;
-    });
-   
+    subtotal = subtotal + priceChange;
+    
     if (subtotal > 499.0) {
         deliveryPrice = 0.0;
     }
@@ -15,7 +69,7 @@ function updateCartTotalPrices(shoppingCartToyDTOs) {
     const total = subtotal + deliveryPrice;
 
     document.getElementById('subtotal').innerText = subtotal + " DKK";
-    document.getElementById('deliveryPrice').innerText = deliveryPrice < 1 && "Gratis levering";
+    document.getElementById('deliveryPrice').innerText = deliveryPrice < 1 ? "Gratis levering" : deliveryPrice;
     document.getElementById('total').innerText = total + " DKK";
 }
 
@@ -25,55 +79,7 @@ function removeToyFromCart() {
     // TODO lav et kald til api/sessionUser med id'et på toy'et som skal fjernes
 
     // i controlleren: 
-        // få fat i sessionUser
-        // delete sessionUser.cart[toyId]
+    // få fat i sessionUser
+    // delete sessionUser.cart[toyId]
 
-}
-
-/*
-var proQty = $('.pro-qty-2');
-proQty.prepend('<span class="fa fa-angle-left dec qtybtn"></span>');
-proQty.append('<span class="fa fa-angle-right inc qtybtn"></span>');
-proQty.on('click', '.qtybtn', function () {
-    var $button = $(this);
-    var oldValue = $button.parent().find('input').val();
-    if ($button.hasClass('inc')) {
-        var newVal = parseFloat(oldValue) + 1;
-    } else {
-        // Don't allow decrementing below zero
-        if (oldValue > 0) {
-            var newVal = parseFloat(oldValue) - 1;
-        } else {
-            newVal = 0;
-        }
-    }
-    $button.parent().find('input').val(newVal);
-});
-*/
-
-
-function incAmount(stockAmount) {
-
-    var amountElement = document.getElementById('chosenAmount');
-    var oldValue = amountElement.value;
-
-    var newVal = oldValue < stockAmount
-        ? parseFloat(oldValue) + 1
-        : oldValue;
-
-    newVal == oldValue && alert(`Der er kun ${oldValue} stk. tilbage af denne vare`);
-
-    amountElement.value = newVal;
-}
-
-function decAmount() {
-
-    var amountElement = document.getElementById('chosenAmount');
-    var oldValue = amountElement.value;
-
-    var newVal = oldValue > 0
-        ? parseFloat(oldValue) - 1
-        : 0;
-
-    amountElement.value = newVal;
 }
