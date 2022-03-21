55e6f3b41b3d413147351dc6be75ec1f0d7236bb
Rasmus Roager <rasm41g0@stud.kea.dk>
2022-03-21 14:00:00 +0100

    Separeret add og remove funktionerne og lavet funktioner til at vise et filledheart og ændre onclick dunktion, når en vare er på wishlist

--- CrazyToys/CrazyToys/wwwroot/scripts/wishlist.js
+++ CrazyToys/CrazyToys/wwwroot/scripts/wishlist.js
@@ -2,32 +2,10 @@
 function addToWishlist(toyId) {
 
     const selectedToy = {
-        toyId: toyId,
+        toyId: toyId
     }
 
-    fetch(`https://localhost:44325/api/sessionuser/AddOrRemoveFromWishlist`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8' // denne linje siger at dataen som vi sender er en string 
-        },
-        body: JSON.stringify(selectedToy)
-    })
-    .then(response => {
-        if (response.ok) {
-            console.log("Toy blev tilføjet/fjernet til/fra ønskelisten");
-        } else {
-            console.log("Toy kunne ikke tilføjet/fjernet til/fra ønskelisten");
-        }
-    });
-}
-
-function removeFromWishlist(toyId) {
-
-    const selectedToy = {
-        toyId: toyId,
-    }
-
-    fetch(`https://localhost:44325/api/sessionuser/AddOrRemoveFromWishlist`, {
+    fetch(`https://localhost:44325/api/sessionuser/AddToyToSessionUsersWishlist`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset=UTF-8' // denne linje siger at dataen som vi sender er en string 
@@ -36,9 +14,116 @@ function removeFromWishlist(toyId) {
     })
         .then(response => {
             if (response.ok) {
-                console.log("Toy blev tilføjet/fjernet til/fra ønskelisten");
+                changeOnclickAndFillHeart(toyId)
+                console.log("Toy blev tilføjet til ønskelisten");
             } else {
-                console.log("Toy kunne ikke tilføjet/fjernet til/fra ønskelisten");
+                console.log("Toy kunne ikke tilføjet til ønskelisten");
             }
         });
+}
+
+function removeFromWishlist(toyId) {
+
+    fetch(`https://localhost:44325/api/sessionuser/RemoveToyFromSessionUsersWishlist?id=${toyId}`, {
+        method: 'DELETE',
+    })
+        .then(response => {
+            if (response.ok) {
+                changeOnclickAndUnfillHeart(toyId)
+                removeChildNodeFromWishlist(toyId);
+                console.log("Toy blev fjernet fra ønskelisten");
+            } else {
+                console.log("Toy kunne ikke fjernet fra ønskelisten");
+            }
+        });
+}
+
+// hvis toyet blev fjernet successfuldt fra sessionUsers wishlist
+// så fjern "wishlist-toy-${toyId}"-elementet fra siden
+function removeChildNodeFromWishlist(toyId) {
+
+    var wishlistToyRow = document.getElementById('wishlist-toy-row');
+
+    var wishlistToy = document.getElementById(`wishlist-toy-${toyId}`);
+
+    // Hvis vi er på shop vil wishlistToy være null
+    if (wishlistToy != null) {
+
+        // Vi fjerner wishlistToy fra siden
+        wishlistToyRow.removeChild(wishlistToy);
+
+        // Hvis der ikke er flere wishlistToys skal tekst ændres
+        if (wishlistToyRow.childElementCount === 0) {
+            var wishlistCount = document.getElementById('wishlist-count');
+
+            wishlistCount.innerText = "Du har ingen produkter på din ønskelisten";
+        }
+    }
+}
+
+function changeOnclickAndFillHeart(toyId) {
+    var wishlistIl = document.getElementById(`wishlist-il-${toyId}`);
+    var wishlistBtn = document.getElementById(`wishlist-btn-${toyId}`);
+
+    if (wishlistBtn != null) {
+
+        // Vi fjerner wishlistBtn og wishlistIcon fra siden
+        wishlistIl.removeChild(wishlistBtn);
+
+        // Laver nye btn og icon elementer
+        var newWishlistBtn = document.createElement('button');
+        var newWishlistIcon = document.createElement('img');
+
+        // Tilføjer attributter til de nye elementer
+        Object.assign(newWishlistBtn, {
+            className: 'wishlist-btn',
+            id: `wishlist-btn-${toyId}`,
+            onclick: function () { removeFromWishlist(toyId) }
+        });
+
+        Object.assign(newWishlistIcon, {
+            className: 'hover-icon',
+            id: `wishlist-icon-${toyId}`,
+            src: 'img/icon/filledheart.png',
+            alt: '""'
+        });
+
+        // Appender de nye elementer
+        newWishlistBtn.appendChild(newWishlistIcon);
+        wishlistIl.appendChild(newWishlistBtn);
+    }
+}
+
+function changeOnclickAndUnfillHeart(toyId) {
+    var wishlistIl = document.getElementById(`wishlist-il-${toyId}`);
+    var wishlistBtn = document.getElementById(`wishlist-btn-${toyId}`);
+
+    if (wishlistBtn != null) {
+
+        // Vi fjerner wishlistBtn og wishlistIcon fra siden
+        wishlistIl.removeChild(wishlistBtn);
+
+        // Laver nye btn og icon elementer
+        var newWishlistBtn = document.createElement('button');
+        var newWishlistIcon = document.createElement('img');
+
+        // Tilføjer attributter til de nye elementer
+        Object.assign(newWishlistBtn, {
+            className: 'wishlist-btn',
+            id: `wishlist-btn-${toyId}`,
+            onclick: function () { addToWishlist(toyId) }
+        });
+
+        Object.assign(newWishlistIcon, {
+            className: 'hover-icon',
+            id: `wishlist-icon-${toyId}`,
+            src: 'img/icon/heart.png',
+            alt: '""'
+        });
+
+        // Appender de nye elementer
+        newWishlistBtn.appendChild(newWishlistIcon);
+        wishlistIl.appendChild(newWishlistBtn);
+    }
+
 }
