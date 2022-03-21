031a38e18e3b457dc53a29ad0d7761f56291d875
TobiasBojesen <tobi103i@stud.kea.dk>
2022-03-21 16:53:37 +0100

    købte produkter bliver nu opdateret i mssql så stock ændres

--- CrazyToys/CrazyToys/Controllers/CheckoutSurfaceController.cs
+++ CrazyToys/CrazyToys/Controllers/CheckoutSurfaceController.cs
@@ -1,6 +1,9 @@
-﻿using CrazyToys.Interfaces;
+﻿using CrazyToys.Entities.Entities;
+using CrazyToys.Interfaces;
+using CrazyToys.Services.EntityDbServices;
 using CrazyToys.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
 using Umbraco.Cms.Core.Routing;
@@ -15,25 +18,45 @@ namespace CrazyToys.Web.Controllers
     public class CheckoutSurfaceController : SurfaceController
     {
         private readonly ISessionService _sessionService;
+        private readonly ToyDbService _toyDbService;
+
 
         public CheckoutSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory,
-            ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, ISessionService sessionService)
+            ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, ISessionService sessionService, ToyDbService toyDbService)
             : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
         {
             _sessionService = sessionService;
+            _toyDbService = toyDbService;
 
         }
 
 
         [HttpPost]
-        public IActionResult Submit(CheckoutUserModel model)
+        public async Task<IActionResult> Submit(CheckoutUserModel model)
         {
             if (!ModelState.IsValid)
             {
                 return CurrentUmbracoPage();
             }
 
-            _sessionService.GetNewOrExistingSessionUser(HttpContext);
+            var sessionUser = _sessionService.GetNewOrExistingSessionUser(HttpContext);
+            foreach (var item in sessionUser.Cart)
+            {
+                Toy toy = await _toyDbService.GetById(item.Key);
+                if(toy.Stock >= item.Value)
+                {
+                    toy.Stock = toy.Stock - item.Value;
+                    await _toyDbService.Update(toy);
+                }
+                else
+                {
+                    toy.Stock = 0;
+                    await _toyDbService.Update(toy);
+                }
+            }
+
+            sessionUser.Cart.Clear();
+            _sessionService.Update(HttpContext, sessionUser);
             // Work with form data here
             //TODO Lav en Order entity Og få hangfire til at kalde en metode der sender email til kunde med en ordre bekfræftelse
 
