8bc84b2f51b1d2c08434c7b0b5d93decdb5af76e
TobiasBojesen <tobi103i@stud.kea.dk>
2022-04-04 09:58:11 +0200

    CategoryDTO oprettet med Properties

---
+++ CrazyToys/CrazyToys.Entities/DTOs/CategoryDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Entities.DTOs
+{
+    public class CategoryDTO
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public List<SubCategoryDTO> subCategoryDTOs { get; set; }
+        public int FoundAmount { get; set; }
+
+        public CategoryDTO(string iD, string name, List<SubCategoryDTO> subCategoryDTOs, int foundAmount)
+        {
+            ID = iD;
+            Name = name;
+            this.subCategoryDTOs = subCategoryDTOs;
+            FoundAmount = foundAmount;
+        }
+    }
+}
