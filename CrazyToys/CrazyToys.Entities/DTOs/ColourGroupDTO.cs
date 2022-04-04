5bf492c93f45769b367c98957c07094c73153044
Rasmus Roager <rasm41g0@stud.kea.dk>
2022-04-04 08:43:13 +0200

    ColourGroupDTO lavet

---
+++ CrazyToys/CrazyToys.Entities/DTOs/ColourGroupDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace CrazyToys.Entities.DTOs
+{
+    public class ColourGroupDTO
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string ColourCode { get; set; }
+
+        public ColourGroupDTO(string id, string name, string colourCode)
+        {
+            ID = id;
+            Name = name;
+            ColourCode = colourCode;
+        }
+    }
+}
