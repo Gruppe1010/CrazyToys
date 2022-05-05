733cf9ce818c97761228c9c01ebac21634744625
VibeJensen <vibej@hotmail.com>
2022-05-05 12:56:35 +0200

    Customer-entitet

--- CrazyToys/CrazyToys.Entities/OrderEntities/Customer.cs
+++ CrazyToys/CrazyToys.Entities/OrderEntities/Customer.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrazyToys.Entities.OrderEntities
 {
-    internal class Customer
+    public class Customer
     {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public IList<Order> Orders { get; set; }
+        public Address BillingAddress { get; set; }
+
+
+
+
+
     }
 }
