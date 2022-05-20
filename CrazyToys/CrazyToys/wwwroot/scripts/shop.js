14581705187812dc46ee0c9a7b1c8622936046cf
VibeJensen <vibej@hotmail.com>
2022-05-20 09:51:21 +0200

    select opdateret til standard Mest Populære

--- CrazyToys/CrazyToys/wwwroot/scripts/shop.js
+++ CrazyToys/CrazyToys/wwwroot/scripts/shop.js
@@ -6,17 +6,23 @@ window.onload = function () {
 function setSorter() {
     // Henter URL og select
     var currentUrl = window.location.href;
+    debugger;
     var sorterSelect = document.getElementById("sorter");
     // Sætter substring
-    var currentUrlSubstring = "sort=price_desc"
+    var priceDesc = "sort=price_desc"
+    var priceAsc = "sort=price_asc"
 
     if (currentUrl.includes("shop?")) {
         // Ændre afhængig af om substring er en del af URL
-        if (currentUrl.includes(currentUrlSubstring)) {
+        if (currentUrl.includes(priceDesc)) {
             sorterSelect.value = "sort=price_desc";
             // Opdatere nice-select value
             $("#sorter").val("sort=price_desc").niceSelect('update');
             
+        } else if (currentUrl.includes(priceAsc)) {
+            sorterSelect.value = "sort=price_asc";
+            // Opdatere nice-select value
+            $("#sorter").val("sort=price_asc").niceSelect('update');
         } else {
             sorterSelect.value = "";
             // Opdaterer nice-select value
