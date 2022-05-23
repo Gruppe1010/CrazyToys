b7d8269380168d0d48c26b9af38f3d8191a8ba86
Rasmus Roager <rasm41g0@stud.kea.dk>
2022-05-23 10:27:58 +0200

    Relaterede produkter er nu synlige på ShopDetails siden med en carousel funktion

--- CrazyToys/CrazyToys/wwwroot/scripts/crazy-toys.js
+++ CrazyToys/CrazyToys/wwwroot/scripts/crazy-toys.js
@@ -60,3 +60,11 @@ function updateCartNumber() {
         })
         .catch(error => console.log);
 }
+
+$('.owl-carousel').owlCarousel({
+    items: 4,
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+})
