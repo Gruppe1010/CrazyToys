ab73ee2a923d4b6bcf18cd97afc0b9d45a244468
VibeJensen <vibej@hotmail.com>
2022-05-27 11:47:09 +0200

    IPaymentService samt QuickPayService oprettet

---
+++ CrazyToys/CrazyToys.Services/QuickPayService.cs
@@ -0,0 +1,24 @@
+﻿using CrazyToys.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyToys.Services
+{
+    public class QuickPayService : IPaymentService
+    {
+        public string CreatePaymentLink(string orderId, string currency)
+        {
+
+            string url = null;
+
+
+
+            return url;
+
+
+        }
+    }
+}
