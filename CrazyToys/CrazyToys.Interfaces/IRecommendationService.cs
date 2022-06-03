c1e59c76c8b6262dbdac03cf7c984d8029566163
VibeJensen <vibej@hotmail.com>
2022-06-03 12:11:02 +0200

    wantedAmount indsat

--- CrazyToys/CrazyToys.Interfaces/IRecommendationService.cs
+++ CrazyToys/CrazyToys.Interfaces/IRecommendationService.cs
@@ -8,7 +8,7 @@ namespace CrazyToys.Interfaces
 {
     public interface IRecommendationService
     {
-        Task<List<ShopToyDTO>> GetRelatedToys(string toyId, int amountToGet);
+        Task<List<ShopToyDTO>> GetRelatedToys(string toyId, int wantedAmount);
 
         Task<List<ShopToyDTO>> GetMostPopularToys(List<Category> categories, int wantedAmount);
     }
